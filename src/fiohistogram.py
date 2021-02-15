7a6b371101faf5b16d05801f3b63a62156ebd21a
Andreas Blanke <blan@itu.dk>
2021-02-15 09:44:59 +0100

    updated readme and added partial loading for fiohistogram

--- src/fiohistogram.py
+++ src/fiohistogram.py
@@ -12,19 +12,25 @@ from sklearn.model_selection import GridSearchCV
 
 from line_count import line_count
 from fio_utils import time_it
+from matplotlib import colors
+from matplotlib.ticker import PercentFormatter
 
-def load(args, filepath): 
-    lc = time_it(line_count, args.verbose, filepath, start_tag="Reading lines in file...", end_tag="Line Count Time")
+def load(args): 
+    lc = time_it(line_count, args.verbose, args.filepath, start_tag="Reading lines in file...", end_tag="Line Count Time")
     print("Lines in file: {}".format(lc))
-    values = np.zeros((lc+1))
+    limit = 1.0 if args.limit < 0.0 or args.limit > 1.0 else args.limit
+    N = int((lc+1) * limit)
+    values = np.zeros(N)
     start_time = time.time_ns()
     if args.verbose:
         print("Starting parsing input...")
-    with open(filepath) as f:
+    with open(args.filepath) as f:
         line_fraction = int(math.ceil(lc / 1E2))
         counter = 0
         time_now = 0
         for i, line in enumerate(f):
+            if i == N:
+                break
             log = re.split(", ", line) 
             # divide by 1000 to go from nsec -> usec, KiB/sec -> MiB/sec or IOPS -> kIOPS 
             values[i] = int(log[1]) / 1000
@@ -38,10 +44,21 @@ def load(args, filepath):
         print("Finished parsing input. Building graph(s)...")
     return values
 
+def color_hist(ax, N, bins, patches): 
+    fracs = N / N.max()
+    norm = colors.Normalize(fracs.min(), fracs.max())
+    for thisfrac, thispatch in zip(fracs, patches):
+        color = plt.cm.viridis(norm(thisfrac))
+        thispatch.set_facecolor(color)
+
 def histogram(args, sample, smin, smax):
     fig, ax = plt.subplots()
-    ax.hist(sample,bins=args.bins, density=True, stacked=True, log=args.ylog, range=(smin, smax))
-    ax.set(xlabel=args.xlabel, ylabel="Propability density",title="Distribution of measurements")
+    N, bins, patches = ax.hist(sample,bins=args.bins, density=True, stacked=True, log=args.ylog, range=(smin, smax))
+    if (args.color):
+        color_hist(ax, N, bins, patches)
+    ax.set(xlabel=args.xlabel, ylabel="Propability density",title="Distribution of measurement values")
+    if (args.percentage):
+        ax.yaxis.set_major_formatter(PercentFormatter(xmax=1,decimals=3))
     if(args.windowed):
         plt.show()
     else:
@@ -67,9 +84,13 @@ def normal_distribution(args, sample, smin, smax):
     if (args.verbose): # plot the histogram and pdf
         print("Plot the histogram and pdf...")
     fig, ax = plt.subplots()
-    ax.hist(sample, bins=args.bins, density=True, stacked=True, log=args.ylog, range=(smin, smax))
+    N, bins, patches = ax.hist(sample, bins=args.bins, density=True, stacked=True, log=args.ylog, range=(smin, smax))
+    if (args.color):
+        color_hist(ax, N, bins, patches)
+    ax.set(xlabel=args.xlabel, ylabel="Propability density",title="Normalized Density Estimation")
+    if (args.percentage):
+        ax.yaxis.set_major_formatter(PercentFormatter(xmax=1,decimals=3))
     ax.plot(values, probabilities)
-    ax.set(xlabel=args.xlabel, ylabel="Propability density",title="Distribution of measurements")
     if(args.windowed):
         plt.show()
     else:
@@ -109,7 +130,11 @@ def kernel_density(args, sample, smin, smax):
     if (args.verbose): # plotting histogram and pdf
         print("Plotting histogram and pdf...")
     fig, ax = plt.subplots()
-    ax.hist(sample, bins=args.bins, density=True, stacked=True, log=args.ylog, range=(smin, smax))
+    N, bins, patches = ax.hist(sample, bins=args.bins, density=True, stacked=True, log=args.ylog, range=(smin, smax))
+    if (args.color):
+        color_hist(ax, N, bins, patches)
+    if (args.percentage):
+        ax.yaxis.set_major_formatter(PercentFormatter(xmax=1,decimals=3))
     ax.plot(values[:], probabilities, label=args.kmode)
     ax.set(xlabel=args.xlabel, ylabel="Propability density",title="Kernel Density Estimation")
     ax.legend()
@@ -121,7 +146,7 @@ def kernel_density(args, sample, smin, smax):
 
 def run(args):
     # load and parse input
-    sample = load(args, args.filepath)
+    sample = load(args)
     if (args.verbose):
         print("File '{}' has been loaded".format(args.filepath))
     
@@ -149,12 +174,15 @@ def main():
     parser.add_argument('-v','--verbose', help="print more information while running script", default=False, action='store_true')
     parser.add_argument('-w','--windowed', help="instead of writing to file then show in window", default=False, action='store_true')
     parser.add_argument('-o','--output', help="filepath for the built graph. defaults to 'output-hist.png'", default="output-hist.png")
+    parser.add_argument('-l','--limit', help="limits the amount of data to be loaded into the graph. provide as decimal percentage. defaults to 1.", default=1.0, type=float)
     parser.add_argument('--xlabel', help="label for x-axis. defaults to 'Latency (usec)'", default="Latency (usec)")
     parser.add_argument('--kmode', help="decide what kernel density estimation method to use. defaults to 'gaussian'", 
                         choices=["gaussian", "tophat", "epanechnikov"], default="gaussian")
     parser.add_argument('--kbw', help="set kernel bandwidth, see: https://en.wikipedia.org/wiki/Kernel_density_estimation#Bandwidth_selection. defaults to '0.1'", default=0.1, type=float)
     parser.add_argument('--bins', help="number of bins to distribute values into. defaults to 100.", default=100)
-    parser.add_argument('--ylog', help="use a logarithmic y-axis", default=False, action='store_true')
+    parser.add_argument('--ylog', help="use a logarithmic y-axis. disabled by default.", default=False, action='store_true')
+    parser.add_argument('--color', help="color the histogram according to height. disabled by default.", default=False, action='store_true')
+    parser.add_argument('--percentage', help="represent probability density by percentage instead of decimal. disabled by default.", default=False, action='store_true')
 
     # prints introduction and measure runtime
     args = parser.parse_args()
