0abb7e64cc528f7b77644f12835ede392e872389
Andreas Blanke <blan@itu.dk>
2021-02-27 13:57:34 +0100

    cleanup and tried to fix scripts in example folder

--- examples/graph-builder.sh
+++ examples/graph-builder.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# Given a folder and arguments to find log files, the graph-io_count-builder will run fiologparser.py on each logfile and generate 
-# graphs that show the IOPS value over time
+# Given a folder and arguments to find log files, the graph-builder will run both fiologparser.py and fiohistogram.py on each logfile and generate 
+# the most typical graphs
 
 # do platform-specific setup
 case "$(uname -s)" in
@@ -15,18 +15,23 @@ esac
 # ask users to provide filepath for folder and pattern strings
 # name pattern for log files: [jobname]_[metric].[jobnumber].log 
 echo "Enter folder containing log files:"
-read HOME_FOLDER    # ex: /D/qd-1/nj8
+read HOME_FOLDER    # /D/qd-1/nj8
 echo "Enter log metric name (ex. bw, lat, iops):"
 read METRIC
 echo "Enter jobname (ex. seq-read):"
 read JOBNAME
 
-# go through log files and run fiologparser.py in io_count mode on each
+# go through log files and run fiohistogram.py on each
 shopt -s nullglob
 files=(${HOME_FOLDER}/*${JOBNAME}_${METRIC}.*.log)
 for f in ${files[@]}
 do 
     name="$(basename -s .log $f)"
+    echo "Processing $name..."
     python3 ../src/fiologparser.py -m io_count -lt "${METRIC}" --title "IOPS distribution over the course of experiment" --every_nth 1000 --same_time -o "$name-iocount.png" -f "$f"
+    python3 ../src/fiologparser.py -m ios -lt "${METRIC}" --title "Measurement value per I/O" -o "$name-ios-ylog.png" -f "$f" -ylog
+    python3 ../src/fiohistogram.py -lt "${METRIC}" -f "$f" -m simple -o "$name-hist.png" --bins 100
+    python3 ../src/fiohistogram.py -lt "${METRIC}" -f "$f" -m simple -o "$name-hist-ylog.png" --bins 100 --ylog
+    echo -e "$name complete\n"
 done
 echo "done"
--- examples/graph-ios-builder.sh
+++ examples/graph-ios-builder.sh
@@ -1,14 +1,33 @@
 #!/bin/sh
+# Given a folder and arguments to find log files, the graph-ios-builder will run fiologparser.py on each logfile and generate 
+# graphs that shows the how metric measurements change over each I/O being performed - with a logarithmic y-axis
+
+# do platform-specific setup
+case "$(uname -s)" in
+    Darwin) echo 'Mac OS X';;
+    Linux) echo 'Linux';;
+    CYGWIN*|MINGW32*|MSYS*|MINGW*)
+        echo 'MS Windows'
+        alias python3=python
+        ;;
+esac
+
+# ask users to provide filepath for folder and pattern strings
+# name pattern for log files: [jobname]_[metric].[jobnumber].log 
 echo "Enter folder containing log files:"
-read HOME_FOLDER    # /D/qd-1/nj8
+read HOME_FOLDER    # ex: /D/qd-1/nj8
 echo "Enter log metric name (ex. bw, lat, iops):"
 read METRIC
 echo "Enter jobname (ex. seq-read):"
 read JOBNAME
+
+# go through log files and run fiologparser.py in io_count mode on each
 shopt -s nullglob
 files=(${HOME_FOLDER}/*${JOBNAME}_${METRIC}.*.log)
 for f in ${files[@]}
 do 
     name="$(basename -s .log $f)"
+    # python3 ../src/fiologparser.py -m ios -lt "${METRIC}" --title "Measurement value per I/O" -o "$name-ios.png" -f "$f"
     python3 ../src/fiologparser.py -m ios -lt "${METRIC}" --title "Measurement value per I/O" -o "$name-ios-ylog.png" -f "$f" -ylog
 done
+echo "done"
