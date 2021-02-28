c942c8c6df52159e14a2141959a1a251e20acddb
Andreas Blanke <blan@itu.dk>
2021-02-28 11:51:26 +0100

    added argument support to example scripts

--- examples/exp-runner.sh
+++ examples/exp-runner.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 # The exp-runner script will look in a given folder, run all experiments found, pack the results into a zip and perform cleanup
-echo "Enter filepath of fio experiment files:"
-read HOME_FOLDER    # ex: /D/qd-1/nj8
+HOME_FOLDER=$1
+if [ -z "${HOME_FOLDER}" ]; then
+    echo "Enter HOME_FOLDER for log files:"
+    read HOME_FOLDER    # /D/qd-1/nj8
+fi
 echo "Starting experimentation"
 shopt -s nullglob
 files=(${HOME_FOLDER}/*.fio)
 for f in ${files[@]}
 do 
 	name="$(basename -s .fio $f)"	# filename without type
+	echo "Executing $name..."
 	fio $f > $name.txt	# run experiment
 	zip -m $name.zip $f $name.txt *.logs # zip all related files and remove them from drive
 	echo "$name has completed."
