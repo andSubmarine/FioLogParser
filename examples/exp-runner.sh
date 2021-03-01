5b7c7447409c20e440dde9a446d903251e954d56
Andreas Blanke <andreas.blanke.dk@gmail.com>
2021-03-01 15:27:03 +0100

    removed test fio and fixed issues with exp-runner.sh

--- examples/exp-runner.sh
+++ examples/exp-runner.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # The exp-runner script will look in a given folder, run all experiments found, pack the results into a zip and perform cleanup
 HOME_FOLDER=$1
 if [ -z "${HOME_FOLDER}" ]; then
@@ -13,8 +13,15 @@ do
 	name="$(basename -s .fio $f)"	# filename without type
 	echo "Executing $name..."
 	fio $f > $name.txt	# run experiment
-	sh graph-builder.sh "$HOME_FOLDER" "lat" "*"
-	zip -m $name.zip $f $name.txt *.log *.png !(output.png) # zip all related files and remove them from drive
+	logfiles=(${HOME_FOLDER}/*_lat.*.log)
+	for lf in ${logfiles[@]}
+	do
+		python3 ../src/fiologparser.py -m io_count -lt lat --title "IOPS distribution over the course of experiment" --every_nth 1000 --same_time -o "$name-iocount.png" -f "$lf"
+		python3 ../src/fiologparser.py -m ios -lt lat --title "Measurement value per I/O" -o "$name-ios-ylog.png" -f "$lf" -ylog
+		python3 ../src/fiohistogram.py -m simple -lt lat -f "$lf" -o "$name-hist.png" --bins 100
+		python3 ../src/fiohistogram.py -m simple -lt lat -f "$lf" -o "$name-hist-ylog.png" --bins 100 --ylog
+	done
+	zip -m $name.zip $f $name.txt *.log $name*.png  # zip all related files and remove them from drive
 	echo "$name has completed."
 done
 echo "Experimentation complete. Please transfer and remove zip files."
