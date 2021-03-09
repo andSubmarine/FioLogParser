cf9a4c4865401c06ea1138ff80feec13f443a545
Andreas Blanke <blan@itu.dk>
2021-03-09 11:32:49 +0100

    set bin width label to round to 3rd decimal

--- src/fiohistogram.py
+++ src/fiohistogram.py
@@ -61,7 +61,7 @@ def color_hist(ax, N, bins, patches):
         thispatch.set_facecolor(color)
 
 def bin_width(args, smin, smax):
-    return "Bin width: {}".format((smax-smin) / int(args.bins))
+    return "Bin width: {}".format(round((smax-smin) / int(args.bins), 3))
 
 def determine_ylabel(args):
     if args.hist_mode == "pdf":
