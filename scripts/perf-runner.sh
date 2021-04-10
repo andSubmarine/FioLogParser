85599553894a92046e908cc72ecf63970b7aef48
Andreas Blanke <blan@itu.dk>
2021-04-10 15:38:14 +0200

    removed sudo from script

--- scripts/perf-runner.sh
+++ scripts/perf-runner.sh
@@ -18,7 +18,7 @@ do
         echo "Executing $name..."
         fio $f > $name.txt &       # run experiment
         mypid=$!
-        sudo perf stat -p $mypid -o $name.perf.txt -e instructions,cpu_clk_unhalted.thread,cs,migrations,faults,frontend_retired.l1i_miss,frontend_retired.l2_miss,offcore_response.demand_code_rd.l3_miss.any_snoop,mem_load_retired.l1_miss,mem_load_retired.l2_miss,mem_load_retired.l3_miss,br_misp_retired.all_branches,dTLB-load-misses,mem_inst_retired.stlb_miss_loads,frontend_retired.stlb_miss,frontend_retired.itlb_miss
+        perf stat -p $mypid -o $name.perf.txt -e instructions,cpu_clk_unhalted.thread,cs,migrations,faults,frontend_retired.l1i_miss,frontend_retired.l2_miss,offcore_response.demand_code_rd.l3_miss.any_snoop,mem_load_retired.l1_miss,mem_load_retired.l2_miss,mem_load_retired.l3_miss,br_misp_retired.all_branches,dTLB-load-misses,mem_inst_retired.stlb_miss_loads,frontend_retired.stlb_miss,frontend_retired.itlb_miss
         echo "$name has completed."
 done
 
