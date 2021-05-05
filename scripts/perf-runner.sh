acb0d0eb254a89af8293113f84dac886ac0f39ff
Andreas Blanke <andreas.blanke.dk@gmail.com>
2021-05-05 13:17:28 +0200

    Update perf-runner.sh

--- scripts/perf-runner.sh
+++ scripts/perf-runner.sh
@@ -19,7 +19,7 @@ do
         fio $f > $name.txt &       # run experiment
         mypid=$!
         sleep 150
-        perf stat -p $mypid -o $name.perf.txt -e instructions,cpu_clk_unhalted.thread,cs,migrations,faults,frontend_retired.l1i_miss,frontend_retired.l2_miss,offcore_response.demand_code_rd.l3_miss.any_snoop,mem_load_retired.l1_miss,mem_load_retired.l2_miss,mem_load_retired.l3_miss,br_misp_retired.all_branches,dTLB-load-misses,mem_inst_retired.stlb_miss_loads,frontend_retired.stlb_miss,frontend_retired.itlb_miss
+        perf stat -p $mypid -o $name.perf.txt -e instructions,cpu_clk_unhalted.thread,cs,migrations,faults,frontend_retired.l1i_miss,frontend_retired.l2_miss,offcore_response.demand_code_rd.l3_miss.any_snoop,mem_load_retired.l1_miss,mem_load_retired.l2_miss,mem_load_retired.l3_miss,br_misp_retired.all_branches,dTLB-load-misses,mem_inst_retired.stlb_miss_loads,frontend_retired.stlb_miss,frontend_retired.itlb_miss sleep 180
         echo "$name has completed."
 done
 
